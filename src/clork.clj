ff2b005bef67140e02d6c698f4cda33ac4b5fd8c
London Clojure Dojo <ldncljdojo@soyuz.(none)>
2010-01-26 20:12:03 +0000

    a little bit further along Bruce2 & Neill

--- src/clork.clj
+++ src/clork.clj
@@ -3,11 +3,27 @@
 ;; represent a room
 ;; represent a set of rooms
 ;; represent monsters
-(def cell {:exits [hall kitchen]
-           :items [bag-of-gold sword]})
+;; (declare hall kitchen bag-of-gold sword)
 
-(def hall {:exits [cell]
-           :items []})
+;; (def cell {:exits [hall kitchen]
+;;            :items [bag-of-gold sword]})
 
+;; (def hall {:exits [cell]
+;;            :items []})
 
+(defstruct player :room)
 
+(defstruct room :north :south :east :west)
+
+(defn west
+  "Move west from from"
+  [from])
+
+(defn north
+  [from])
+
+(defn south
+  [from])
+
+(defn east
+  [from])
