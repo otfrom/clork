a24e3cf01038af0c2310a7af64fbd5f53023b2c4
London Clojure Dojo <ldncljdojo@soyuz.(none)>
2010-01-26 20:26:36 +0000

    looking at rooms Bruce2 & Andrew

--- src/clork.clj
+++ src/clork.clj
@@ -5,19 +5,37 @@
 ;; represent monsters
 ;; (declare hall kitchen bag-of-gold sword)
 
+
+
 ;; (def cell {:exits [hall kitchen]
 ;;            :items [bag-of-gold sword]})
 
 ;; (def hall {:exits [cell]
 ;;            :items []})
 
+(def rooms {:hall {:description "A large vaulted hall"
+                   :exits {:n :kitchen
+                           :w :lounge}}
+            :kitchen {:description "A kitchern with a roaring fire"
+                      :exits {:s :hall}
+                      }
+            :lounge {:description "A lounge with a red chez longue"
+                     :exits {:e :hall}}
+           })
+
+(defn look [room]
+(:description (room rooms))
+  )
+
 (defstruct player :room)
 
-(defstruct room :north :south :east :west)
+
 
 (defn west
   "Move west from from"
   [from])
+
+
 
 (defn north
   [from])
