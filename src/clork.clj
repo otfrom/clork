ad5472e2c8fd0534359a2799eecfd07efe25319c
London Clojure Dojo <ldncljdojo@soyuz.(none)>
2010-01-26 20:32:48 +0000

    start of moving Andrew & Robert

--- src/clork.clj
+++ src/clork.clj
@@ -16,32 +16,13 @@
 (def rooms {:hall {:description "A large vaulted hall"
                    :exits {:n :kitchen
                            :w :lounge}}
-            :kitchen {:description "A kitchern with a roaring fire"
-                      :exits {:s :hall}
-                      }
+            :kitchen {:description "A kitchen with a roaring fire"
+                      :exits {:s :hall}}
             :lounge {:description "A lounge with a red chez longue"
-                     :exits {:e :hall}}
-           })
+                     :exits {:e :hall}}})
 
-(defn look [room]
-(:description (room rooms))
-  )
+(defn look [rooms room]
+  (:description (room rooms)))
 
-(defstruct player :room)
-
-
-
-(defn west
-  "Move west from from"
-  [from])
-
-
-
-(defn north
-  [from])
-
-(defn south
-  [from])
-
-(defn east
-  [from])
+(defn move [rooms from direction]
+  (direction (:exits (rooms from))))
