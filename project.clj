ac27732f4ac505e8bff8981ca8c5529345517bb0
Tom Crayford <tcrayford@googlemail.com>
2010-02-05 23:06:09 +0000

    added swank as a dependancy

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject clork "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.1.0"] [org.clojure/clojure-contrib "1.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
+  :dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]])
