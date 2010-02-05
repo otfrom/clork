965c0b5772c5222ec4a4320d9f4cdbf1d15bfcc3
Tom Crayford <tcrayford@googlemail.com>
2010-02-05 23:06:50 +0000

    added move-test

---
+++ test/clork_test.clj
@@ -0,0 +1,12 @@
+(ns clork-test
+  (:use clojure.test clork))
+
+(defn clork-fixture [f]
+  (def test-rooms {:test-room-1 {:exits {:w :test-room-2}}})
+  (f)
+  (def test-rooms nil))
+
+(use-fixtures :once clork-fixture)
+
+(deftest move-test
+  (is (= (move-player {:location :test-room-1} test-rooms :w) {:location :test-room-2})))
