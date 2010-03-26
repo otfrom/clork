0150db093cca4783b685aa9775a5a85946778dd3
Bruce Durling <bld@soyuz.(none)>
2010-03-26 22:21:19 +0000

    Updated README to have a basic description of the project and updated project.clj to point to more recent versions of the clojure and lein swank jars.

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject clork "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.1.0"] [org.clojure/clojure-contrib "1.0-SNAPSHOT"]])
+  :description "Clork text adventure of the London Clojure dojo #ldncljdojo"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]]
+  :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]])
