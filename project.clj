df20447ba60a29f42d3d3534c9aef334026f1baa
Bruce Durling <bld@soyuz.(none)>
2010-05-16 15:00:16 +0100

    Moved to lein-swank 1.1.0 in an effort to move to clojure 1.2.

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "Clork text adventure of the London Clojure dojo #ldncljdojo"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]])
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
