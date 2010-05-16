1876c3f264d840cbf77feeeb838826fcdbfefbf2
Bruce Durling <bld@soyuz.(none)>
2010-05-16 19:53:18 +0100

    Created a better world.

--- src/world.clj
+++ src/world.clj
@@ -1,10 +1,18 @@
-(ns world)
+(ns world
+  (:use clork))
 
-(def rooms {:hall {:description "A large vaulted hall"
-                   :exits {:n :kitchen
-                           :w :lounge}}
-            :kitchen {:description "A kitchen with a roaring fire"
-                      :exits {:s :hall}}
-            :lounge {:description "A lounge with a red chez longue"
-                     :exits {:e :hall}}})
+(def *world* {:rooms {:hall (struct room
+                                    {:w :kitchen
+                                     :n :study}
+                                    "hall"
+                                    [:sword])
+                      :kitchen (struct room
+                                       {:e :hall}
+                                       "kitchen")
+                      :study (struct room
+                                     {:s :hall}
+                                     "study")}
+              :players {:player1 (struct player :hall [])
+                        :player2 (struct player :kitchen [])}
+              :items {:sword (struct item "A very pointy sword.")}})
 
