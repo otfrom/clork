79e3e353aaaaf8d0c479744a023c00be2d3c0c7f
Bruce Durling <bld@soyuz.(none)>
2010-05-16 16:30:30 +0100

    A minimal 1.2 project.clj that works with clojure-mode 1.7.1 and clojure-test-mode 1.4.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject clork "0.0.3-SNAPSHOT"
   :description "Clork text adventure of the London Clojure dojo #ldncljdojo"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.2.0"]]
+  :repositories {"clojure-releases" "http://build.clojure.org/releases"})
